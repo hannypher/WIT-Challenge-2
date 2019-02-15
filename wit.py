c2700c01b3e6ca53a8b93966bfc110c7489dd175
hannypher <hannypherneenah@gmail.com>
2019-02-15 18:12:56 +0300

    i have added another code

---
+++ wit.py
@@ -0,0 +1,16 @@
+print("Hanifa's Age Categories\n")
+
+print("")
+try:
+    year= int(input("Please enter your date of birth: (numbers)\n"))
+    age = 2019 - year
+    age= int(age)
+    if age <18:
+        print("Minor")
+    if age >=18 and age <=36:
+        print("Youth")
+    if age >=36:
+        print("Elder")
+
+except:
+    print("please write in figures or numbers")
